e487925f8183bdf20c01a43de96034fd0372a579
lonord <lonord@qq.com>
2018-03-03 23:39:37 +0800

    更新代码，支持新的接口

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import Main from './main'
 
+// tslint:disable-next-line:no-var-requires
+const pkg = require('../package.json')
+
 export const size = 'normal'
 export const Comp = Main
+export const version = pkg.version
+export const name = '示例模块'
